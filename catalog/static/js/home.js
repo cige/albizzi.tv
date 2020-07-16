3f5d6566682e12c20e60e5215701d0e2785c492d
clementgeorge <clementyj.george@gmail.com>
2020-07-16 14:15:33 +0200

    client-side filtering system enhancement

--- catalog/static/js/home.js
+++ catalog/static/js/home.js
@@ -1,53 +1,88 @@
 var browser
 
+const FILTERS = [
+    {
+        key: "period",
+        label: "Par période"
+    },
+    {
+        key: "composer",
+        label: "Par compositeur"
+    },
+    {
+        key: "interpreter",
+        label: "Par interprète"
+    }
+]
+
+/**
+ * Instanciate the Vue.js model
+ * @param {*} el the DOM element containing the Vue.js template
+ * @param {*} clips A list of media clips
+ */
 function initBrowser(el, clips){
-    browser = new Vue({
-        delimiters: ['[[', ']]'],
-        el,
-        data: {
-            clips,
-            filteredClips: clips,
-            filterinterpreter: null,
-            filtercomposer: null
+    const data = {
+        clips,
+        filteredClips: clips,
+        ... FILTERS.reduce((acc, f) => {
+            acc[f.key] = null
+            return acc
+        }, {})
+    }
+
+    const computed = {
+        /** Compute whether a filter is currently active */
+        isFiltered: function(){
+            return FILTERS.some(f => this[f] !== null)
         },
-        computed: {
-            interpreters: function(){
-                return uniqValuesOf('interpreter')(this.clips).sort().map(interpreter => {
+        /** 
+         *  For each filters, create a computed list containing all unique values for such a filter.
+         *  For instance, if we have a filter named 'composer', it will create a computed list named 'composers'
+         *  containing all unique composers in the list of media. Furthermore, it will associate for each composer
+         *  its number of occurence within the filtering context, and whether it's the active filter
+         */
+        ... FILTERS.reduce((acc, f) => {
+            acc[f.key + 's'] = function(){
+                return uniqValuesOf(f.key)(this.clips).sort().map(value => {
                     return {
-                        name: interpreter,
-                        nb: this.filteredClips.reduce((acc, c) => c['interpreter'] == interpreter ? acc + 1 : acc, 0),
-                        active: this.filterinterpreter == interpreter
+                        name: value,
+                        nb: this.filteredClips.reduce((acc, clip) => clip[f.key] == value ? acc + 1 : acc, 0),
+                        active: this[f.key] == value
                     }
                 })
-            },
-            composers: function(){
-                return uniqValuesOf('composer')(this.clips).sort().map(composer => {
-                    return {
-                        name: composer,
-                        nb: this.filteredClips.reduce((acc, c) => c['composer'] == composer ? acc + 1 : acc, 0),
-                        active: this.filtercomposer == composer
-                    }
-                })
-            },
-            isFiltered: function(){
-                return this.filtercomposer !== null || this.filterinterpreter !== null
             }
+            return acc
+        }, {})
+    }
+
+    const methods = {
+        applyFilter: function(filter,value){
+            this[filter.key] = value
+            this.filteredClips = this.clips.filter(clip => FILTERS.every(f => {
+                return this[f.key] === null // or the filter is 'off'
+                || this[f.key] == clip[f.key] // or both values match
+            }))
         },
-        methods: {
-            filterWith: function(field,value){
-                this['filter' + field] = value
-                this.filteredClips = this.clips.filter(c => (!this.filterinterpreter || c['interpreter'] == this.filterinterpreter) 
-                    && (!this.filtercomposer || c['composer'] == this.filtercomposer))
-            },
-            reset: function(){
-                this.filtercomposer = null
-                this.filterinterpreter = null
-                this.filteredClips = this.clips
-            }
+        reset: function(){
+            FILTERS.forEach(filter => this[filter.key] = null)
+            this.filteredClips = this.clips
+        },
+        values: function(filter){
+            return this[filter.key + 's']
         }
-      });
+    }
+    browser = new Vue({
+        delimiters: ['[[', ']]'], // We have to change default Vue.js delimiters as they conflict wih Django brackets
+        el,
+        data,
+        computed,
+        methods })
 }
 
+/**
+ * Given a field name, and a list of media clips, returns the list of unique values
+ * found within that list. 
+ */
 const uniqValuesOf = field => clips => {
     return (clips || []).reduce((values, current) => {
         const v = current[field]
